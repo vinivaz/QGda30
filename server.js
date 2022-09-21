a99f4e777510835e44dd33187f8b2d7cb9d1e82b
GabrielVaz7 <viniciusvaz.tech@gmail.com>
2022-09-21 16:46:43 -0300

    some cors tests

--- server.js
+++ server.js
@@ -10,7 +10,22 @@ const port = process.env.PORT || 3333;
 const app = express();
 
 app
-.use(cors())
+.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
+// .use(cors())
 .use(express.json({limit: '50mb'}))
 .use(express.urlencoded({ extended: true, limit : '50mb' }))
 
